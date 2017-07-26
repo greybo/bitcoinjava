acd778c3e006f0b18db3a151a732931e97680921
Sergey B <sergeybotl@gmail.com>
2017-07-26 17:48:08 +0000

    Config.java edited online with Bitbucket

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -3,5 +3,5 @@ package util;
 import exmo.Exmo;
 
 public class Config {
-    public static Exmo exmo = new Exmo("K-3e85f38ed3ce354a357ce13953953f90242d9bbd", "S-c87c32ae21bb72eed3f6e94dcc8d1c4869b6dbb8");
+    public static Exmo exmo = new Exmo();
 }
